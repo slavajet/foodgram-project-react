b371d641351160877f5c74652f4964a15cdf7c25
slavajet <slavacaas@yandex.ru>
2023-09-19 18:22:16 +0600

    добавил модель RecipeIngredient навел порядок

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -4,37 +4,6 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-class Recipe(models.Model):
-    name = models.CharField(max_length=200, verbose_name='Название')
-    text = models.TextField(verbose_name='Описание')
-    cooking_time = models.IntegerField(verbose_name='Время приготовления в минутах')
-    image = models.URLField(blank=True, null=True, verbose_name='Изображение')
-    author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='recipes',
-        verbose_name='Автор'
-    )
-    tags = models.ManyToManyField(
-        'Tag',
-        related_name='recipes',
-        verbose_name='Теги'
-    )
-    ingredients = models.ManyToManyField(
-        'Ingredient',
-        through='RecipeIngredient',
-        related_name='recipes',
-        verbose_name='Ингредиенты'
-    )
-    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        ordering = ('-pub_date',)
-
-
 class Tag(models.Model):
     name = models.CharField(max_length=200, verbose_name='Название')
     color = models.CharField(
@@ -59,7 +28,6 @@ class Tag(models.Model):
         verbose_name = 'Тег'
         verbose_name_plural = 'Теги'
 
-
 class Ingredient(models.Model):
     name = models.CharField(max_length=200, verbose_name='Название')
     measurement_unit = models.CharField(max_length=200, verbose_name='Единица измерения')
@@ -71,3 +39,56 @@ class Ingredient(models.Model):
         ordering = ('name',)
         verbose_name = 'Ингредиент'
         verbose_name_plural = 'Ингредиенты'
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Название')
+    text = models.TextField(verbose_name='Описание')
+    cooking_time = models.IntegerField(verbose_name='Время приготовления в минутах')
+    image = models.URLField(blank=True, null=True, verbose_name='Изображение')
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipes',
+        verbose_name='Автор'
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        related_name='recipes',
+        verbose_name='Теги'
+    )
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        through='RecipeIngredient',
+        related_name='recipes',
+        verbose_name='Ингредиенты'
+    )
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('-pub_date',)
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='recipe_ingredients',
+        verbose_name='Рецепт'
+    )
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+        verbose_name='Ингредиент'
+    )
+    amount = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.recipe.name} - {self.ingredient.name}"
+
+    class Meta:
+        verbose_name = 'Ингредиент рецепта'
+        verbose_name_plural = 'Ингредиенты рецепта'
