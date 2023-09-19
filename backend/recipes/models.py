a6f10e23a2ea4a386f13468083acc22f8553f23c
slavajet <slavacaas@yandex.ru>
2023-09-19 17:26:19 +0600

    поправил модель reciep

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -4,15 +4,31 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class Recipe(models.Model):
-    name = models.CharField(max_length=255)
-    text = models.TextField()
-    cooking_time = models.IntegerField()
-    image = models.URLField(blank=True, null=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipes')
-    is_favorited = models.BooleanField(default=False)
-    is_in_shopping_cart = models.BooleanField(default=False)
-    tags = models.ManyToManyField('Tag', related_name='recipes')
-    ingredients = models.ManyToManyField('Ingredient', through='RecipeIngredient', related_name='recipes')
+    name = models.CharField('Название', max_length=200)
+    text = models.TextField('Описание')
+    cooking_time = models.IntegerField('Время приготовления в минутах')
+    image = models.URLField('Изображение', blank=True, null=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipes',
+        verbose_name='Автор'
+    )
+    tags = models.ManyToManyField(
+        'Tag',
+        related_name='recipes',
+        verbose_name='Теги'
+    )
+    ingredients = models.ManyToManyField(
+        'Ingredient',
+        through='RecipeIngredient',
+        related_name='recipes',
+        verbose_name='Ингредиенты'
+    )
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        ordering = ('-pub_date',)
