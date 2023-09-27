a15f281505c0d80941eb2b3b1a637d1a448b1333
slavajet <slavacaas@yandex.ru>
2023-09-27 15:57:55 +0600

    Добавил валидацию данных

--- backend/api/validation.py
+++ backend/api/validation.py
@@ -4,29 +4,29 @@ from rest_framework import serializers
 
 def validate_tags(tags):
     if not tags:
-        raise serializers.ValidationError("Tags list cannot be empty.")
+        raise serializers.ValidationError("Теги не могут быть пустыми.")
     tag_ids = set()
     for tag in tags:
         if tag.id in tag_ids:
-            raise serializers.ValidationError("Duplicate tags are not allowed.")
+            raise serializers.ValidationError("Нельзя использовать одинаковые теги.")
         tag_ids.add(tag.id)
 
 
 def validate_cooking_time(cooking_time):
     if cooking_time is not None and cooking_time < 1:
-        raise serializers.ValidationError("Cooking time must be 1 or greater.")
+        raise serializers.ValidationError("Время приготовления должно быть 1 или больше.")
 
 
 def validate_ingredients(ingredients):
     if not ingredients:
-        raise serializers.ValidationError("Ingredients list cannot be empty.")
+        raise serializers.ValidationError("Нельзя приготовить что либо без ингредиентов.")
     ingredient_ids = set()
     for ingredient_data in ingredients:
         ingredient_id = ingredient_data.get('id')
         if ingredient_id in ingredient_ids:
-            raise serializers.ValidationError("Duplicate ingredients are not allowed.")
+            raise serializers.ValidationError("Ингредиенты не должны повторяться.")
         ingredient_ids.add(ingredient_id)
 
         amount = ingredient_data.get('amount')
         if amount is None or amount < 1:
-            raise serializers.ValidationError("Ingredient amount must be 1 or greater.")
+            raise serializers.ValidationError("Игредиентов должно быть 1 или больше.")
